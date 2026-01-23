26b863624e973eb0e19c2919ffe9530a26818bfe
Asheq mahmud <128743584+noobhacker32003@users.noreply.github.com>
2026-01-24 05:22:15 +0600

    using range

--- lab2/scores.go
+++ lab2/scores.go
@@ -1,9 +1,19 @@
+// package tournament
+
+// func CalculateTotal(scores [5]int) int {
+// 	total := 0
+// 	for i := 0; i < 5; i++ {
+// 		total = total + scores[i]
+// 	}
+// 	return total
+// }
+
 package tournament
 
 func CalculateTotal(scores [5]int) int {
 	total := 0
-	for i := 0; i < 5; i++ {
-		total = total + scores[i]
+	for _, score := range scores {
+		total += score
 	}
 	return total
 }
