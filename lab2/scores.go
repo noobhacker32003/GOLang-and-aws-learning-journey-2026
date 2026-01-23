ea43a507c6d151e326c59c69cd07f230ee240d86
Asheq mahmud <128743584+noobhacker32003@users.noreply.github.com>
2026-01-24 05:28:47 +0600

    added slice over array

--- lab2/scores.go
+++ lab2/scores.go
@@ -8,9 +8,19 @@
 // 	return total
 // }
 
+// package tournament
+
+// func CalculateTotal(scores [5]int) int {
+// 	total := 0
+// 	for _, score := range scores {
+// 		total += score
+// 	}
+// 	return total
+// }
+
 package tournament
 
-func CalculateTotal(scores [5]int) int {
+func CalculateTotal(scores []int) int {
 	total := 0
 	for _, score := range scores {
 		total += score
--- lab2/scores_test.go
+++ lab2/scores_test.go
@@ -2,12 +2,35 @@ package tournament
 
 import "testing"
 
-func TestCalculateTotal(t *testing.T) {
-	scores := [5]int{10, 15, 20, 12, 18}
-	got := CalculateTotal(scores)
-	want := 75
+// func TestCalculateTotal(t *testing.T) {
+// 	scores := [5]int{10, 15, 20, 12, 18}
+// 	got := CalculateTotal(scores)
+// 	want := 75
 
-	if got != want {
-		t.Errorf("got %d but wanted %d", got, want)
-	}
+// 	if got != want {
+// 		t.Errorf("got %d but wanted %d", got, want)
+// 	}
+// }
+
+
+func TestCalculateTotalSlice(t *testing.T) {
+	t.Run("with 5 scores", func(t *testing.T) {
+		scores := []int{10, 15, 20, 12, 18}
+		got := CalculateTotal(scores)
+		want := 75
+		
+		if got != want {
+			t.Errorf("got %d but wanted %d", got, want)
+		}
+	})
+	
+	t.Run("with 3 scores", func(t *testing.T) {
+		scores := []int{25, 30, 35}
+		got := CalculateTotal(scores)
+		want := 90
+		
+		if got != want {
+			t.Errorf("got %d but wanted %d", got, want)
+		}
+	})
 }
